ff2065ddfae44cfa5863cb015dfc742eeb639683
keyanbagheri <keyanbagheri@gmail.com>
2017-04-17 16:21:08 +0800

    moving user.swift to models folder

--- InstaClone/User.swift
+++ InstaClone/User.swift
@@ -1,22 +1,33 @@
 //
 //  User.swift
-//  
+//  SimpleFirebase
 //
-//  Created by bitbender on 4/15/17.
-//
+//  Created by bitbender on 4/6/17.
+//  Copyright © 2017 Key. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
-class User: NSObject {
-    var id: String?
-    var name: String?
-    var email: String?
-    var profileImageUrl: String?
-    init(dictionary: [String: AnyObject]) {
-        self.id = dictionary["id"] as? String
-        self.name = dictionary["name"] as? String
-        self.email = dictionary["email"] as? String
-        self.profileImageUrl = dictionary["profileImageUrl"] as? String
+class User {
+    var id: String
+    var email : String
+    var screenName : String
+    var desc : String
+    var imageURL : String
+    
+    init( ) {
+        id = ""
+        email = ""
+        screenName = ""
+        desc = ""
+        imageURL = ""
+    }
+    
+    init(anId : String, anEmail : String, aScreenName : String, aDesc : String, anImageURL : String) {
+        id = anId
+        email = anEmail
+        screenName = aScreenName
+        desc = aDesc
+        imageURL = anImageURL
     }
 }
